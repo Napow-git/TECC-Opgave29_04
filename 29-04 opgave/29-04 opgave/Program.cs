506fe61fdc4f06aa44115ca218a30dd8329b0d65
Napow-Git <37689142+Napow-git@users.noreply.github.com>
2024-04-29 13:58:07 +0200

    Add files via upload

---
+++ 29-04 opgave/29-04 opgave/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+
+namespace _29_04_opgave
+{
+
+    public class Bill
+    {
+        
+
+        private int _year;
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        public decimal Pris()
+
+        {
+            return 100000m;
+        }
+  
+    }
+               
+}
+
+
+
+
+
